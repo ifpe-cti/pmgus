609ee9489aa452bf5b4156e80008f8d512955e7e
Vinícius Henrique <viniciussoaresti@gmail.com>
2018-08-16 17:47:01 -0300

    arquivos comuns

---
+++ src/main/java/com/github/viniciussoaresti/infraestrutura/repositorios/implementacoes/FabricaRepositorios.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.viniciussoaresti.infraestrutura.repositorios.implementacoes;
+
+import com.github.viniciussoaresti.infraestrutura.repositorios.comportamentos.RepositorioGenerico;
+
+/**
+ *
+ * @author Vinícius Soares
+ */
+public class FabricaRepositorios {
+    public static final int ARMA = 1;
+    public static final int DROGA = 2;
+    public static final int OCORRENCIA = 3;
+    
+     public static final int MEMORIA= 1;
+      public static final int BANCODADOS= 2;
+       public static final int ARQUIVO= 3;
+       
+    public static RepositorioGenerico manufactor (int tipoNegocio, int tipoPersistencia){
+        if(tipoPersistencia == MEMORIA){
+            if(tipoNegocio == ARMA){
+                return new RepositorioArma();
+            }
+            if(tipoNegocio == DROGA){
+                return new RepositorioDroga();
+            }
+            if(tipoNegocio == OCORRENCIA){
+                return new RepositorioOcorrencia();
+            }
+        }
+        
+        if(tipoPersistencia == BANCODADOS){
+            if(tipoNegocio == ARMA){
+                return null;
+            }
+            if(tipoNegocio == DROGA){
+                return null;
+            }
+            if(tipoNegocio == OCORRENCIA){
+                return null;
+            }    
+        }
+       
+          if(tipoPersistencia == ARQUIVO){
+            if(tipoNegocio == ARMA){
+                return null;
+            }
+            if(tipoNegocio == DROGA){
+                return null;
+            }
+            if(tipoNegocio == OCORRENCIA){
+                return null;
+            }             
+    }
+          return null;
+    
+}
+}
