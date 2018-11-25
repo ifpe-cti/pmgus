8798219af1c4bf1fa5fc75bcea1471923830dcbb
viniciussoaresti <viniciussoaresti@gmail.com>
2018-11-25 11:32:18 -0200

    teste do excel pegando

--- src/main/java/excel/AbreExcel.java
+++ src/main/java/excel/AbreExcel.java
@@ -1,88 +1,70 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package excel;
-import com.github.viniciussoaresti.pmgus.controladores.ArmaController;
+
 import com.github.viniciussoaresti.pmgus.negocio.Arma;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-  
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
-  
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class AbreExcel {
-  
-       private static final String fileName = "C:\\Users\\barro.DESKTOP-RV892QA\\Documents\\teste1.xls";
-  
-       public static void main(String[] args) throws IOException {
-  
-            List<Arma> listaArma = new ArrayList<Arma>();
-            ArmaController armaC = new ArmaController();
-             try {
-                    FileInputStream arquivo = new FileInputStream(new File(
-                                  AbreExcel.fileName));
-  
-                    HSSFWorkbook workbook = new HSSFWorkbook(arquivo);
-  
-                    HSSFSheet sheetArma = workbook.getSheetAt(0);
-  
-                    Iterator<Row> rowIterator = sheetArma.iterator();
-  
-                    while (rowIterator.hasNext()) {
-                        int rownum = 1;
-                           Row row = rowIterator.next();
-                           Iterator<Cell> cellIterator = row.cellIterator();
-  
-                           Arma arma = new Arma();
-                           listaArma.add(arma);
-                           
-                           while (cellIterator.hasNext()) {
-                                  Cell cell = cellIterator.next();
-                                  switch (cell.getColumnIndex()) {
-                                  case 0:
-                                        arma.setCodigo(Integer.valueOf(cell.getCellType().toString()));
-                                        break;
-                                  case 1:
-                                        arma.setTipoDeArma(cell.getStringCellValue());
-                                        break;
-                                  case 2:
-                                        arma.setModelo(cell.getStringCellValue());
-                                        break;
-                                  case 3:
-                                        arma.setMarca(cell.getStringCellValue());
-                                        break;
-                                  case 4:
-                                         arma.setCalibre(Integer.valueOf(cell.getCellType().toString()));
-                                        break;
-                                 
-                                  }
-                           }
+
+    private static final String teste = "src/main/java/excel/teste.xlsx";
+
+    public static void main(String[] args) throws IOException {
+        List<Arma> armas = new ArrayList<>();
+        //ArmaController armaC = new ArmaController();
+        try {
+            OPCPackage pkg = OPCPackage.open(new File(teste));
+            XSSFWorkbook wb = new XSSFWorkbook(pkg);
+            for (Sheet sheet : wb) {
+                for (int i = 0; i < sheet.getLastRowNum(); i++) { //linha
+                    Row linha = null;
+                    if(sheet.getRow(i)!=null){
+                    linha = sheet.getRow(i);
                     }
-                    arquivo.close();
-  
-             } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                    System.out.println("Arquivo Excel não encontrado!");
-             }
-  
-             if (listaArma.size() == 0) {
-                    System.out.println("Nenhum aluno encontrado!");
-             } else {
-                    
-                    for (Arma arma : listaArma) {
-   
-                          System.out.println("Arma: " + arma.getMarca());
- 
-                    }                    
-             }
-       }
+                    if (linha!=null&&linha.getCell(i)!=null&&!linha.getCell(0).getCellType().equals(CellType.BLANK)) {
+                        Arma arma = new Arma();
+                        for (Cell celula : linha) { //coluna
+                            switch (celula.getColumnIndex()) {
+                                case 0: //lembrar q o código é automatico quando definir no banco
+                                    arma.setCodigo((int) celula.getNumericCellValue());
+                                    break;
+                                case 1:
+                                    arma.setTipoDeArma(celula.getStringCellValue());
+                                    break;
+                                case 2:
+                                    arma.setModelo(celula.getStringCellValue());
+                                    break;
+                                case 3:
+                                    arma.setMarca(celula.getStringCellValue());
+                                    break;
+                                case 4:
+                                    arma.setCalibre((int) celula.getNumericCellValue());
+                                    break;
+                            }
+                        }
+                        armas.add(arma);
+                    }
+                }
+            }
+            pkg.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (armas.size() == 0) {
+            System.out.println("Nenhuma arma encontrada!");
+        } else {
+            int a = 0;
+            for (Arma arma : armas) {
+                System.out.println("Arma" + a + ": " + arma.toString());
+                a++;
+            }
+        }
+    }
 }
