4136b0d3f95e9ccb20c62b8b18b409ca1a50cea0
Barros Pedro <pedro1812.ps@gmail.com>
2018-11-28 10:35:23 -0200

    Com fachada

--- src/main/java/com/github/viniciussoaresti/pmgus/controladores/VitimaController.java
+++ src/main/java/com/github/viniciussoaresti/pmgus/controladores/VitimaController.java
@@ -9,6 +9,7 @@ package com.github.viniciussoaresti.pmgus.controladores;
 import com.github.viniciussoaresti.pmgus.infraestrutura.repositorios.comportamentos.RepositorioGenerico;
 import com.github.viniciussoaresti.pmgus.infraestrutura.repositorios.implementacoes.RepositorioVitimaImplBD;
 import com.github.viniciussoaresti.pmgus.negocio.Vitima;
+import java.io.Serializable;
 import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -22,7 +23,7 @@ import javax.faces.context.FacesContext;
 
 @ManagedBean
 @SessionScoped
-public class VitimaController {
+public class VitimaController implements Serializable{
      private RepositorioGenerico<Vitima,Integer> repositorioVitima = null;
     private Vitima vitimaCadastro;
     private Vitima selectedVitima;
