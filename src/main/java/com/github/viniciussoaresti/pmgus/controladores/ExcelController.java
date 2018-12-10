f0a141717b94a8bd061d8fe28844b21485ba31db
viniciussoaresti <viniciussoaresti@gmail.com>
2018-12-10 02:44:55 -0200

    aceitando cadastro de drogas

--- src/main/java/com/github/viniciussoaresti/pmgus/controladores/ExcelController.java
+++ src/main/java/com/github/viniciussoaresti/pmgus/controladores/ExcelController.java
@@ -1,6 +1,6 @@
 package com.github.viniciussoaresti.pmgus.controladores;
 
-import com.github.viniciussoaresti.pmgus.negocio.Arma;
+import com.github.viniciussoaresti.pmgus.negocio.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -32,6 +32,8 @@ import org.primefaces.model.UploadedFile;
 public class ExcelController {
 
     List<Arma> armas;
+    List<TipoDroga> tiposDroga;
+    List<UnidadeDroga> unidadesDroga;
 
     public void cadastrarArmas(ArmaController armacontroller) {
         JFileChooser arquivo = new JFileChooser();
@@ -45,8 +47,8 @@ public class ExcelController {
                 FacesContext.getCurrentInstance().addMessage(null,
                         new FacesMessage(FacesMessage.SEVERITY_INFO, "Erro!", "As armas não foram cadastradas!"));
             } else {
-                  FacesContext.getCurrentInstance().addMessage(null,
-                  new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", "O upload foi realizado com sucesso!"));
+                FacesContext.getCurrentInstance().addMessage(null,
+                        new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", "O upload foi realizado com sucesso!"));
                 OPCPackage pkg = OPCPackage.open(caminho);
                 XSSFWorkbook wb = new XSSFWorkbook(pkg);
                 for (Sheet sheet : wb) {
@@ -59,16 +61,16 @@ public class ExcelController {
                             Arma arma = new Arma();
                             for (Cell celula : linha) { //coluna
                                 switch (celula.getColumnIndex()) {
-                                    case 1:
+                                    case 2:
                                         arma.setTipoDeArma(celula.getStringCellValue());
                                         break;
-                                    case 2:
+                                    case 3:
                                         arma.setModelo(celula.getStringCellValue());
                                         break;
-                                    case 3:
+                                    case 4:
                                         arma.setMarca(celula.getStringCellValue());
                                         break;
-                                    case 4:
+                                    case 5:
                                         arma.setCalibre(Double.toString(celula.getNumericCellValue()));
                                         break;
                                 }
@@ -78,13 +80,90 @@ public class ExcelController {
                     }
                 }
                 for (Arma a : armas) {
-                    armacontroller.setArmaCadastro(a);
-                    armacontroller.inserir();
+                    if (!armacontroller.recuperarTodosArmas()
+                            .parallelStream()
+                            .anyMatch(arma -> {
+                                return arma.equals(a);
+                            })) {
+                        armacontroller.setArmaCadastro(a);
+                        armacontroller.inserir();
+                    }
                 }
                 pkg.close();
-                FacesContext.getCurrentInstance().getExternalContext().redirect("crudArma.xhtml");
                 FacesContext.getCurrentInstance().addMessage(null,
                         new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", "As armas foram cadastradas com sucesso!"));
+                FacesContext.getCurrentInstance().getExternalContext().redirect("crudArma.xhtml");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void cadastrarDrogas(TipoDrogaController tdc, UnidadeDrogaController udc) {
+        JFileChooser arquivo = new JFileChooser();
+        tiposDroga = new ArrayList<>();
+        unidadesDroga = new ArrayList<>();
+        String caminho = null;
+        try {
+            if (arquivo.showDialog(null, "Enviar") == JFileChooser.APPROVE_OPTION) {
+                caminho = arquivo.getSelectedFile().getAbsolutePath();
+            }
+            if (caminho == null) {
+                FacesContext.getCurrentInstance().addMessage(null,
+                        new FacesMessage(FacesMessage.SEVERITY_INFO, "Erro!", "As drogas não foram cadastradas!"));
+            } else {
+                FacesContext.getCurrentInstance().addMessage(null,
+                        new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", "O upload foi realizado com sucesso!"));
+                OPCPackage pkg = OPCPackage.open(caminho);
+                XSSFWorkbook wb = new XSSFWorkbook(pkg);
+                for (Sheet sheet : wb) {
+                    for (int i = 0; i < sheet.getLastRowNum(); i++) { //linha
+                        Row linha = null;
+                        if (sheet.getRow(i) != null) {
+                            linha = sheet.getRow(i);
+                        }
+                        if (linha != null && linha.getCell(i) != null && !linha.getCell(0).getCellType().equals(CellType.BLANK)) {
+                            TipoDroga tipo = new TipoDroga();
+                            UnidadeDroga unidade = new UnidadeDroga();
+                            for (Cell celula : linha) { //coluna
+                                switch (celula.getColumnIndex()) {
+                                    case 4:
+                                        tipo.setNome(celula.getStringCellValue());
+                                        break;
+                                    case 5:
+                                        unidade.setUnidadeDroga(celula.getStringCellValue());
+                                        break;
+                                }
+                            }
+                            tiposDroga.add(tipo);
+                            unidadesDroga.add(unidade);
+                        }
+                    }
+                }
+                for (TipoDroga t : tiposDroga) {
+                    if(!tdc.recuperarTodosTipoDroga()
+                            .parallelStream()
+                            .anyMatch(tipo -> {
+                                return tipo.equals(t);
+                            })){
+                        tdc.setTipoDrogaCadastro(t);
+                        tdc.inserir();
+                    }
+                }
+                for (UnidadeDroga u : unidadesDroga) {
+                    if(!udc.recuperarTodosUnidadeDroga()
+                            .parallelStream()
+                            .anyMatch(unidade -> {
+                                return unidade.equals(u);
+                            })) {
+                        udc.setUnidadeDrogaCadastro(u);
+                        udc.inserir();
+                    }
+                }
+                pkg.close();
+                FacesContext.getCurrentInstance().addMessage(null,
+                        new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", "As armas foram cadastradas com sucesso!"));
+                FacesContext.getCurrentInstance().getExternalContext().redirect("crudTipoDroga.xhtml");
             }
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/com/github/viniciussoaresti/pmgus/controladores/RelatorioController.java
+++ src/main/java/com/github/viniciussoaresti/pmgus/controladores/RelatorioController.java
@@ -5,12 +5,10 @@
  */
 package com.github.viniciussoaresti.pmgus.controladores;
 
-import com.github.viniciussoaresti.pmgus.infraestrutura.dao.PersistenceDao;
 import java.util.Date;
 import java.util.List;
 import com.github.viniciussoaresti.pmgus.negocio.*;
 import java.io.Serializable;
-import java.util.ArrayList;
 import javax.faces.bean.ManagedBean;
 
 /**
@@ -20,76 +18,10 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean
 public class RelatorioController implements Serializable {
 
-    //filtros de arma, droga, turno, bairro
-    List<Arma> armas;
-    List<TipoDroga> enderecos;
-    List<Ocorrencia> ocorrencia;
+    //filtros de arma, droga, turno, bairro -> ocorrencias, mapa e gráficos
+    private List<Ocorrencia> ocorrencias;
     private Arma selectedArma;
-    private TipoDroga selectedTipoDroga;
-    private Ocorrencia selectedOcorrencia;
-
-    /*public void armasTest() {
-        helper.execute(Arma.class, streams -> {
-            armas = streams.
-                    where(b -> b.getCodigo()==selectedArma.getCodigo())
-                    .toList();
-        });
-    }*/
-    public void armasTest() {
-        Arma a = new Arma(1, "1", "1", "1", "1");
-        armas = new ArrayList<>();
-        armas.add(a);
-       try{
-           armas = (List<Arma>)PersistenceDao.getInstance().read("select a from Arma a where a.codigo="+selectedArma.getCodigo());
-       }catch(IndexOutOfBoundsException index){
-       }
-    }
-
-    public List<Arma> getArmas() {
-        return armas;
-    }
-
-    public void setArmas(List<Arma> armas) {
-        this.armas = armas;
-    }
-
-    public List<TipoDroga> getEnderecos() {
-        return enderecos;
-    }
-
-    public void setEnderecos(List<TipoDroga> enderecos) {
-        this.enderecos = enderecos;
-    }
-
-    public List<Ocorrencia> getOcorrencia() {
-        return ocorrencia;
-    }
-
-    public void setOcorrencia(List<Ocorrencia> ocorrencia) {
-        this.ocorrencia = ocorrencia;
-    }
-
-    public Arma getSelectedArma() {
-        return selectedArma;
-    }
-
-    public void setSelectedArma(Arma selectedArma) {
-        this.selectedArma = selectedArma;
-    }
-
-    public TipoDroga getSelectedTipoDroga() {
-        return selectedTipoDroga;
-    }
-
-    public void setSelectedTipoDroga(TipoDroga selectedTipoDroga) {
-        this.selectedTipoDroga = selectedTipoDroga;
-    }
-
-    public Ocorrencia getSelectedOcorrencia() {
-        return selectedOcorrencia;
-    }
-
-    public void setSelectedOcorrencia(Ocorrencia selectedOcorrencia) {
-        this.selectedOcorrencia = selectedOcorrencia;
-    }
+    private TipoDroga selectedDroga;
+    private Date selectedDate;
+    
 }
